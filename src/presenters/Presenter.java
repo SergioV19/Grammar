d4428b8890f49871531abf705fcb8b74ea5a3e24
JhonatanD09 <joranada@gmail.com>
2021-08-16 15:21:32 -0500

    validaciones de la gramatica

--- src/presenters/Presenter.java
+++ src/presenters/Presenter.java
@@ -29,16 +29,26 @@ public class Presenter implements ActionListener {
 			ArrayList<String> noTerminals = convert(framePrincipal.getNoTerminals());
 			String axiomatic = framePrincipal.getAxiomatic();
 			ArrayList<Production> productions = convertProductions(framePrincipal.getProductions());
-			if (valideSymbolsInProductions(terminals, noTerminals, productions)) {
+			if (valideSymbolsInProductions(terminals, noTerminals, productions) && valideAxiomaticSymbol(noTerminals,axiomatic)) {
 				grammar = new Grammar(terminals, noTerminals, axiomatic, productions);
 				grammar.showTree();
 				framePrincipal.resetJTextFile();
 			} else {
 				JOptionPane.showMessageDialog(null,
-						"Los simbolos de las producciones no coinciden con los simbolos terminales o no terminales");
+						"Los simbolos de las producciones no coinciden con los simbolos terminales o no terminales o no hay una produccion que "
+						+ "inicie con el simbolo inicial axomatico","Error en la gramatica",JOptionPane.ERROR_MESSAGE);
 			}
 			break;
 		}
+	}
+
+	private boolean valideAxiomaticSymbol(ArrayList<String> noTerminals, String axiomatic) {
+		for (String noTerminalsTemp : noTerminals) {
+			if (axiomatic.equals(noTerminalsTemp)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	private boolean valideSymbolsInProductions(ArrayList<String> terminals, ArrayList<String> noTerminals,
@@ -46,8 +56,9 @@ public class Presenter implements ActionListener {
 		boolean temp = false;
 		for (Production production : productions) {
 			for (int i = 0; i < production.getProduction().length(); i++) {
-				if (valideTerminalsAndNoTerminals(terminals, "" + production.getProduction().charAt(i))
-						|| valideTerminalsAndNoTerminals(noTerminals, "" + production.getProduction().charAt(i))) {
+				if ((valideTerminalsAndNoTerminals(terminals, "" + production.getProduction().charAt(i))
+						|| valideTerminalsAndNoTerminals(noTerminals, "" + production.getProduction().charAt(i)))
+						&& valideAxiomaticSymbol(noTerminals, production.getProducer())) {
 					temp =  true;
 				}else {
 					temp = false;
@@ -79,7 +90,11 @@ public class Presenter implements ActionListener {
 		ArrayList<Production> productions = new ArrayList<Production>();
 		ArrayList<String> temp = convert(text);
 		for (String p : temp) {
+			if (p.split(">>").length<2) {
+			JOptionPane.showMessageDialog(null,"Hay una produccion incompleta", "Error en las producciones",JOptionPane.ERROR_MESSAGE);	
+			}else {
 			productions.add(new Production(p.split(">>")[0], p.split(">>")[1]));
+			}
 		}
 		return productions;
 	}
